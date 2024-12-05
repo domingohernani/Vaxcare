0b392ca9fcbd4b0a2eabcad1ba900c2af1d9a95e
domingohernani <dev.hernani.domingo@gmail.com>
2024-12-05 22:37:37 +0800

    navigation

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/vaxcare_logo.png";
+import header1 from "../assets/generalIcons/header-1.png";
+import header2 from "../assets/generalIcons/header-2.png";
+import header3 from "../assets/generalIcons/header-3.png";
+import logoutIcon from "../assets/generalIcons/logout.png";
 
 export default function NavBar() {
   console.log("NavBar was rendered");
@@ -34,45 +38,27 @@ export default function NavBar() {
   };
 
   return (
-    <nav className="flex flex-row items-center px-8 py-3 border-b bg-primary ">
-      <div className="w-1/6 logo">
-        <img src={logo} alt="Vaxcare" className="w-16 h-2w-16" />
-      </div>
-      <div className="z-30 flex flex-row justify-end flex-1 gap-2 mr-3">
-        <div
-          className="relative flex items-center justify-center cursor-pointer w-9 h-9"
-          onClick={logOut}
-        >
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              opacity="0.1"
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M3 12C3 4.5885 4.5885 3 12 3C19.4115 3 21 4.5885 21 12C21 16.3106 20.4627 18.6515 18.5549 19.8557L18.2395 18.878C17.9043 17.6699 17.2931 16.8681 16.262 16.3834C15.2532 15.9092 13.8644 15.75 12 15.75C10.134 15.75 8.74481 15.922 7.73554 16.4097C6.70593 16.9073 6.09582 17.7207 5.7608 18.927L5.45019 19.8589C3.53829 18.6556 3 16.3144 3 12ZM8.75 10C8.75 8.20507 10.2051 6.75 12 6.75C13.7949 6.75 15.25 8.20507 15.25 10C15.25 11.7949 13.7949 13.25 12 13.25C10.2051 13.25 8.75 11.7949 8.75 10Z"
-              fill="#323232"
-            />
-            <path
-              d="M3 12C3 4.5885 4.5885 3 12 3C19.4115 3 21 4.5885 21 12C21 19.4115 19.4115 21 12 21C4.5885 21 3 19.4115 3 12Z"
-              stroke="#323232"
-              strokeWidth="2"
-            />
-            <path
-              d="M15 10C15 11.6569 13.6569 13 12 13C10.3431 13 9 11.6569 9 10C9 8.34315 10.3431 7 12 7C13.6569 7 15 8.34315 15 10Z"
-              stroke="#323232"
-              strokeWidth="2"
-            />
-            <path
-              d="M6 19C6.63819 16.6928 8.27998 16 12 16C15.72 16 17.3618 16.6425 18 18.9497"
-              stroke="#323232"
-              strokeWidth="2"
-              strokeLinecap="round"
-            />
-          </svg>
+    <nav className="flex flex-row items-center justify-between px-8 py-3 border-b bg-red-30 bg-primary ">
+      <section className="flex items-center gap-4">
+        <div className="logo">
+          <img src={logo} alt="Vaxcare" className="w-16 h-auto" />
         </div>
+        <img src={header1} className="w-20 h-auto" />
+      </section>
+      <section className="flex items-center gap-8 text-white">
+        <img src={header2} className="w-20 h-auto" />
+        <div>
+          <span className="text-3xl font-extrabold">Pag kumpleto,</span> <br />
+          <span className="text-3xl font-extrabold">Protektado</span>
+        </div>
+        <img src={header3} className="w-20 h-auto" />
+      </section>
+      <div
+        className="relative flex items-center justify-center w-12 h-12 rounded-full cursor-pointer bg-red"
+        style={{ backgroundColor: "rgba(255, 0, 0, 0.59)" }}
+        onClick={logOut}
+      >
+        <img src={logoutIcon} className="w-5 h-auto" />
       </div>
     </nav>
   );
